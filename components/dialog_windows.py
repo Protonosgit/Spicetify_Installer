3503ba24dc4ef680900f0930e61bcaa9577639cf
Protonos <linuslinz98@gmail.com>
2024-01-20 19:39:03 +0100

    Some functional/visual improvements to uninstall modal

--- components/dialog_windows.py
+++ components/dialog_windows.py
@@ -1,5 +1,5 @@
 import os
-from PyQt6.QtWidgets import QDialog
+from PyQt6.QtWidgets import QDialog, QDialogButtonBox
 from PyQt6.QtCore import pyqtSlot
 from PyQt6.uic import loadUi
 
@@ -33,8 +33,50 @@ class UninstallWindow(QDialog):
             parent_directory, "res", "uninstall.ui")
         loadUi(pathmaster, self)
 
-        self.accepted.connect(self.onAccept)
+        self.deacmods = False
+        self.remmods = False
+        self.unispicetify = False
+        self.meisterpropper = False
 
-    @pyqtSlot()
-    def onAccept(self):
-        print("Accept button clicked!")
+        self.bt_cancel.clicked.connect(self.close)
+        self.bt_apply.clicked.connect(self.ApplyChanges)
+
+        self.check1.stateChanged.connect(self.option1)
+        self.check2.stateChanged.connect(self.option2)
+        self.check3.stateChanged.connect(self.option3)
+        self.check4.stateChanged.connect(self.option4)
+
+    def option1(self):
+
+        self.warning.setStyleSheet("color: rgba(0, 0, 0, 0);")
+        self.bt_apply.setEnabled(True)
+        self.deacmods = self.check1.isChecked()
+
+    def option2(self):
+        self.warning.setStyleSheet("color: red")
+        self.bt_apply.setEnabled(True)
+        self.check1.setChecked(False)
+        self.check1.setEnabled(not self.check2.isChecked())
+        self.remmods = self.check2.isChecked()
+
+    def option3(self):
+        self.warning.setStyleSheet("color: red")
+        self.bt_apply.setEnabled(True)
+        self.unispicetify = self.check3.isChecked()
+
+    def option4(self):
+        self.warning.setStyleSheet("color: red")
+        self.bt_apply.setEnabled(True)
+        self.check1.setChecked(False)
+        self.check2.setChecked(False)
+        self.check3.setChecked(False)
+        self.check1.setEnabled(not self.check4.isChecked())
+        self.check2.setEnabled(not self.check4.isChecked())
+        self.check3.setEnabled(not self.check4.isChecked())
+        self.meisterpropper = self.check4.isChecked()
+
+    def ApplyChanges(self):
+
+        print(self.deacmods, self.remmods,
+              self.unispicetify, self.meisterpropper)
+        self.close()
